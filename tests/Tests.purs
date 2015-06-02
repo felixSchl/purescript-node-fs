69e440ef086df0faae8be4e98770f9ae50b3dda1
Felix Schlitter <felixschlitter@gmail.com>
2015-06-02 21:46:58 +1200

    Add tests for Node.FS.exists

--- tests/Tests.purs
+++ tests/Tests.purs
@@ -1,14 +1,33 @@
 module Tests where
 
-import Prelude
-import Control.Monad.Eff
-import qualified Node.FS as FS
-import qualified Test.Mocha as Mocha
+import Debug.Trace (trace)
+import Test.Assert.Simple (assertEqual, assertBool)
 import Test.Mocha (
     Describe(..), It(..), Done(..)
+  , beforeEach
   , describe, it, itAsync, itIs, itIsNot
 )
 
-main = describe "a test with mocha" $ do
-    itAsync "passes" \done -> do
+import Data.Either(either)
+import Data.Foldable(foldl)
+import Control.Monad.Eff.Exception(throwException)
+import qualified Node.Path as Path
+import qualified Node.FS.Async as FS.Async
+
+resolveFixture = Path.resolve [ "tests", "fixtures" ]
+
+main = describe "Node.FS.Async" do
+
+  describe ".exists" do
+    let fixture = resolveFixture "test-exists"
+        resolve = Path.resolve [ fixture ]
+
+    itAsync "returns `true` if the path exists " \done -> do
+      FS.Async.exists (resolve "a")  \exists -> do
+        assertEqual exists true
+        itIs done
+
+    itAsync "returns `false` if the path does not exists " \done -> do
+      FS.Async.exists (resolve "b") \exists -> do
+        assertEqual exists false
         itIs done
